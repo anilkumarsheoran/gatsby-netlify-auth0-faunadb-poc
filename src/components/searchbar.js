f564b873fbd943ad7abbc4875a216ebc4c1e57ea
anil_sheoran <anilkumar.sheoran@gmail.com>
2021-11-06 00:49:16 +1100

    key issue fix

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -5,12 +5,9 @@ import {
     Hits,
     SearchBox,
   } from 'react-instantsearch-dom';
-//   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/reset-min.css" integrity="sha256-t2ATOGCtAIZNnzER679jwcFcKYfLlw01gli6F6oszk8=" crossorigin="anonymous">
 
   
-//const { InstantSearch, SearchBox, Hits } = ReactInstantSearchDOM;
-
-const searchClient = algoliasearch('88IVVBSS0A', '4a4c241d16358f8c4f2de8e1819a3065');
+const searchClient = algoliasearch(process.env.GATSBY_YOUR_ALGOLIA_APP_ID, process.env.GATSBY_YOUR_ALGOLIA_SEARCH_KEY);
 
 export default function Searchbar() {
   return <InstantSearch searchClient={searchClient} indexName="blog-post">
